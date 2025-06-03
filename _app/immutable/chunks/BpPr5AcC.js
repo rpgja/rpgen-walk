ad4b986bc44d2ba0b5874a47d8f7323363691ed2
onjmin <onjmin@users.noreply.github.com>
2025-06-03 13:51:41 +0000

    deploy: 6b25779aa510f1c75393f3a35bfe8226957f22a0

--- _app/immutable/chunks/BpPr5AcC.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1l3o1p6)==null?void 0:s.base)??"/rpgen-walk";var a;const l=((a=globalThis.__sveltekit_1l3o1p6)==null?void 0:a.assets)??e;export{l as a,e as b};
