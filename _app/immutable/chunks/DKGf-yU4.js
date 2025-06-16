39765ffddb86c198eca904c8542504c68212f530
onjmin <onjmin@users.noreply.github.com>
2025-06-16 13:44:08 +0000

    deploy: 89b10aba086c160d2fe635faec619fab7561c44d

---
+++ _app/immutable/chunks/DKGf-yU4.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1p3c54k)==null?void 0:s.base)??"/rpgen-walk";var a;const t=((a=globalThis.__sveltekit_1p3c54k)==null?void 0:a.assets)??e;export{t as a,e as b};
