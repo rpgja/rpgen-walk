45a4c22b822bdaf4d58cb9a07831d1b8dfbed044
onjmin <onjmin@users.noreply.github.com>
2025-06-17 14:37:48 +0000

    deploy: 624d08c66f77810f35162ae818abc720761b1032

---
+++ _app/immutable/chunks/Bn4t1DuT.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_ek7i0l)==null?void 0:s.base)??"/rpgen-walk";var e;const l=((e=globalThis.__sveltekit_ek7i0l)==null?void 0:e.assets)??a;export{l as a,a as b};
